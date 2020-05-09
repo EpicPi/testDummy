768cd643968b7dd0e3adc25d57bf44c188767c69
EpicPi <piyushgk1@gmail.com>
2020-05-09 11:55:11 -0400

    move back to multiple browsers

--- test.js
+++ test.js
@@ -16,37 +16,19 @@ async function runDummyUser() {
         .headless()
         )
       .build();
-    // open n+1 tabs
     await await driver.get('https://test.gatherly.io');
-    for( let i = 0; i < 9; i++){
-      await driver.executeScript("window.open('https://test.gatherly.io')");
-    }
-    const handles = await driver.getAllWindowHandles();
-    console.log(handles);
-    for(let tab of handles){
-      await driver.switchTo().window(tab);
-      let mediaButton = await driver.wait(until.elementLocated(By.id("get_media_button")),10000);
-      mediaButton.click();
-      let nameButton = await driver.wait(until.elementLocated(By.id("name_button")),10000);
-      nameButton = await driver.wait(until.elementIsVisible(nameButton),1000);
-      nameButton.click();
-      await driver.sleep(100);
-    }
-    console.log('no erros yet');
-    // 4 directions and 'alt' for a non movement one 
+    let mediaButton = await driver.wait(until.elementLocated(By.id("get_media_button")),10000);
+    mediaButton.click();
+    let nameButton = await driver.wait(until.elementLocated(By.id("name_button")),10000);
+    nameButton = await driver.wait(until.elementIsVisible(nameButton),1000);
+    nameButton.click();
+    await driver.sleep(100);
+    // 4 directions and 'enter' for a non movement one 
     const actions = [Key.ENTER, Key.ARROW_LEFT, Key.ARROW_DOWN, Key.ARROW_UP, Key.ARROW_RIGHT];
-    const map = {};
     while(true){
-      const handles = await driver.getAllWindowHandles();
-      for(let tab of handles){
-        await driver.switchTo().window(tab);
-        if(map[tab]){
-          await driver.actions().keyUp(actions[map[tab]]).perform();
-        }
-        const direction = Math.floor(Math.random()* 5);
-        map[tab] = direction;
-        await driver.actions().keyDown(actions[map[tab]]).perform();
-      }
+      const direction = Math.floor(Math.random()* 5);
+      await driver.actions().keyDown(actions[direction]).perform();
+      await driver.actions().keyUp(actions[direction]).perform();
       await driver.sleep(509);
     }
   }catch(error){
@@ -55,4 +37,6 @@ async function runDummyUser() {
   }
 }
 
-runDummyUser();
+for(let i =0; i<20; i++){
+  runDummyUser();
+}
