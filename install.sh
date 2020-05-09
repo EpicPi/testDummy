13a63ca86caf08f7186872bfdfa23257117565f3
EpicPi <piyushgk1@gmail.com>
2020-05-09 14:19:53 -0400

    forgot to install node

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 apt-get update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh -y
-npm i -g chromedriver
+nvm install node -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 npm install
+npm i -g chromedriver
